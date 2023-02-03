bc3d7da91c0b4659731bef904a00122bba23e4a3
Guilherme Raposo <gui.raposo36@gmail.com>
2023-02-02 21:13:31 -0300

    feat: first commit and create weather app toolbar

---
+++ layouts/default.vue
@@ -0,0 +1,78 @@
+<template>
+  <v-app dark>
+    <v-navigation-drawer
+      v-model="drawer"
+      :mini-variant="miniVariant"
+      :clipped="clipped"
+      fixed
+      app
+    >
+      <v-list>
+        <v-list-item
+          v-for="(item, i) in items"
+          :key="i"
+          :to="item.to"
+          router
+          exact
+        >
+          <v-list-item-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <v-app-bar color="indigo" dark :clipped-left="clipped" fixed app>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+      <v-toolbar-title>{{ title }}</v-toolbar-title>
+      <v-spacer />
+      <v-btn plain>
+        <n-link class="link" to="/weather-app">Weather App</n-link>
+      </v-btn>
+    </v-app-bar>
+
+    <v-main>
+      <v-container>
+        <Nuxt />
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'DefaultLayout',
+  data() {
+    return {
+      clipped: false,
+      drawer: false,
+      fixed: false,
+      items: [
+        {
+          icon: 'mdi-home',
+          title: 'Home',
+          to: '/',
+        },
+        {
+          icon: 'mdi-cloud',
+          title: 'Weather App',
+          to: '/weather-app',
+        },
+      ],
+      miniVariant: false,
+      right: true,
+      rightDrawer: false,
+      title: 'Learning Nuxt',
+    }
+  },
+}
+</script>
+
+<style scoped>
+.link {
+  text-decoration: none;
+  color: #fff;
+}
+</style>
