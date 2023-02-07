4dd5f881908c11ef69f86773ce9655a534d5ba56
Guilherme Raposo <gui.raposo36@gmail.com>
2023-02-06 21:43:03 -0300

    feat: Dynamic City Weather

--- pages/weather-app.vue
+++ pages/weather-app.vue
@@ -1,24 +1,44 @@
 <template>
   <v-container>
     <!-- <v-layout> -->
-      <h1 class="display-1 text-center">Weather App</h1>
+    <h1 class="display-1 text-center">Weather App</h1>
 
-      <v-flex xs12>
-        <v-card color="blue-grey darken-2">
-          <v-card-text class="white--text">Here is the card</v-card-text>
-        </v-card>
-      </v-flex>
+    <v-flex xs12>
+      <v-card color="blue-grey darken-2">
+        <v-card-text class="white--text">Here is the card</v-card-text>
+      </v-card>
+    </v-flex>
 
-      <v-flex xs12 class="mt-4">
-        <v-text-field solo label="Enter City Name" />
-      </v-flex>
+    <v-flex xs12 class="mt-4">
+      <v-form @submit.prevent="getWeatherInfo">
+        <v-text-field v-model="city" solo label="Enter City Name" />
+      </v-form>
+    </v-flex>
     <!-- </v-layout> -->
   </v-container>
 </template>
 
 <script>
-import { defineComponent } from 'vue'
-export default defineComponent({})
+export default {
+  data() {
+    return {
+      city: 'London',
+      weather: {}
+    }
+  },
+  created() {
+    this.getWeatherInfo()
+  },
+  methods: {
+    getWeatherInfo() {
+      this.$axios
+        .$get(
+          `https://api.openweathermap.org/data/2.5/weather?q=${this.city}&APPID=bede080e9ccd38476d8ea25dfa9f91cc`
+        )
+        .then((res) => (this.weather = res))
+    }
+  }
+}
 </script>
 
 <style scoped></style>
