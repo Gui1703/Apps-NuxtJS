90f8d7ef67eb0eb10a3fb3ae886844680c81879a
Guilherme Raposo <gui.raposo36@gmail.com>
2023-03-01 18:49:35 -0300

    feat: updated

--- store/index.js
+++ store/index.js
@@ -14,12 +14,12 @@ export const getters = {
   }
 }
 
-export const actions = {
-  nuxtServerInit({ commit }, { req, app: { $cookies } }) {
-    if (req.headers.cookie) {
-      if ($cookies.get('token')) {
-        commit('auth/setLoggedIn', true)
-      }
-    }
-  }
-}
+// export const actions = {
+//   nuxtServerInit({ commit }, { req, app: { $cookies } }) {
+//     if (req.headers.cookie) {
+//       if ($cookies.get('token')) {
+//         commit('auth/setLoggedIn', true)
+//       }
+//     }
+//   }
+// }
